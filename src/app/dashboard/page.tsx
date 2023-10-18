2790eb9bc82fd3aa456ce3631e8e1f33c06946ce
nahuelvera00 <nahuelvera3770@gmail.com>
2023-10-18 16:43:25 -0300

    added and config tRPC

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+
+const Page = async () => {
+  const { getUser } = await getKindeServerSession();
+  const user = await getUser();
+
+  if (!user || !user.id) redirect("/auth-callback?origin=dashboard");
+
+  return <div>{user.email}</div>;
+};
+
+export default Page;
---
+++ src/trpc/index.ts
@@ -0,0 +1,18 @@
+import { publicProcedure, router } from "./trpc";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { TRPCError } from "@trpc/server";
+
+export const appRouter = router({
+  authCallback: publicProcedure.query(() => {
+    const { getUser } = getKindeServerSession();
+    const user = getUser();
+
+    if (!user.id || !user.email) throw new TRPCError({ code: "UNAUTHORIZED" });
+
+    // Check if the user is in  the database
+
+    return { success: true };
+  }),
+});
+
+export type appRouter = typeof appRouter;
