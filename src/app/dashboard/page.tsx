15aa5e2ef3e0eb5959f443eea7ec1402632e58b2
nahuelvera00 <nahuelvera3770@gmail.com>
2023-10-19 14:09:41 -0300

    added auth-callback page and funtions

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,9 @@
-"use client";
-
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 
 const Page = async () => {
-  const { getUser } = await getKindeServerSession();
-  const user = await getUser();
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
 
   if (!user || !user.id) redirect("/auth-callback?origin=dashboard");
 
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,15 +1,31 @@
+import { db } from "@/db";
 import { publicProcedure, router } from "./trpc";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { TRPCError } from "@trpc/server";
 
 export const appRouter = router({
-  authCallback: publicProcedure.query(() => {
+  authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession();
     const user = getUser();
 
     if (!user.id || !user.email) throw new TRPCError({ code: "UNAUTHORIZED" });
 
-    // Check if the user is in  the database
+    // check if the user is in the database
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id,
+      },
+    });
+
+    if (!dbUser) {
+      // create user in db
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+        },
+      });
+    }
 
     return { success: true };
   }),
